29985eb3a68153f045bec8896884749802fb5020
Kwaku Appiah-Adu <kwakuappiahadu@gmail.com>
2020-07-20 13:48:00 +0000

    Added JWT Refreshing to API

--- models/item.py
+++ models/item.py
@@ -15,14 +15,22 @@ class ItemModel(db.Model):
         self.name = name
         self.price = price
         self.store_id = store_id
-        # self.id = id
 
     def json(self):
-        return {'name': self.name, 'price': self.price}
+        return {
+            'id': self.id,
+            'name': self.name,
+            'price': self.price,
+            'store_id': self.store_id
+        }
 
     @classmethod
     def find_by_name(cls, name):
         return cls.query.filter_by(name=name).first()
+
+    @classmethod
+    def find_all(cls):
+        return cls.query.all()
 
     def save_to_db(self):
         db.session.add(self)
